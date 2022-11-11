17bb5bb90d11fec74984e758aa506fbd8b2a2caa
monaemhmila <61699407+monaemhmila@users.noreply.github.com>
2022-11-11 21:19:01 +0100

    login interface
    first action increating interfaces

---
+++ logininterface/logininterface/logininterfaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  logininterfaceApp.swift
+//  logininterface
+//
+//  Created by Jasser,monaem on 11/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct logininterfaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
